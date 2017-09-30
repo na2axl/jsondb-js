6262aeba936745ab6b3eb6fcda0228c3c3d3132e
Axel Nana <ax.lnana@outlook.com>
2017-09-30 13:10:30 +0200

    TablePrototype

---
+++ ts/types/TablePrototype.ts
@@ -0,0 +1,31 @@
+/**
+ * JSONDB - JSON Database Manager
+ *
+ * Manage JSON files as databases with JSONDB Query Language (JQL)
+ *
+ * This content is released under the BSD-3-Clause License
+ *
+ * Copyright (c) 2016-2017, Alien Technologies
+ *
+ * @package    JSONDB
+ * @author     Nana Axel <ax.lnana@outlook.com>
+ * @copyright  Copyright (c) 2016-2017, Alien Technologies
+ * @license    https://spdx.org/licenses/BSD-3-Clause.html BSD 3-clause "New" or "Revised" License
+ * @file       TablePrototype.ts
+ */
+
+/**
+ * Provides a base interface for table's prototypes.
+ */
+export interface TablePrototype
+{
+    [row_name: string]: {
+        type: "int" | "integer" | "number" | "decimal" | "float" | "string" | "char" | "bool" | "boolean" | "array",
+        default?: number | string | boolean | Array<any>,
+        max_length?: number,
+        not_null?: boolean,
+        auto_increment?: boolean,
+        primary_key?: boolean,
+        unique_key?: boolean
+    }
+}
